3c40e660ca05cc2ce313d1d7312aa59861cd499a
mattviolet <mattviolet@gmail.com>
2020-06-09 14:44:37 -0700

    GET data from Dexcom API

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,91 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+
+const tokens = require('../../tokens');
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props) {
+    super(props);
+    this.state = {
+      authorizationCode: '',
+      accessToken: '',
+      userBgData: {}
+    };
+    this.navToDexcomLogin = this.navToDexcomLogin.bind(this);
+  }
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  componentDidMount = () => {
+    const { authorizationCode } = this.state;
+    if (!authorizationCode) {
+      this.saveUserAuth();
+    }
+    this.obtainAccessToken();
+  }
+
+  saveUserAuth = () => {
+    const userAuth = window.location.href.replace('http://localhost:3000/?code=', '');
+    if (userAuth.length) {
+      this.state.authorizationCode = userAuth;
+    }
+  }
+
+  navToDexcomLogin = () => {
+    const dexcomLoginUrl = `https://api.dexcom.com/v2/oauth2/login?client_id=${tokens.client_id}&redirect_uri=${tokens.redirect_uri}&response_type=code&scope=offline_access`;
+    window.location.href = dexcomLoginUrl;
+  }
+
+  obtainAccessToken = () => {
+    const that = this;
+    const { authorizationCode } = this.state;
+    const data = `client_secret=${tokens.client_secret}&client_id=${tokens.client_id}&code=${authorizationCode}&grant_type=authorization_code&redirect_uri=${tokens.redirect_uri}`;
+
+    const req = new XMLHttpRequest();
+    req.withCredentials = true;
+
+    req.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        const res = JSON.parse(this.responseText);
+        that.setState({
+          accessToken: res.access_token
+        });
+      }
+    });
+    req.open('POST', 'https://api.dexcom.com/v2/oauth2/token');
+    req.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    req.setRequestHeader('cache-control', 'no-cache');
+
+    req.send(data);
+  }
+
+  requestUserBgData = () => {
+    const that = this;
+    const data = null;
+    const { accessToken } = this.state;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        const res = JSON.parse(this.responseText);
+        that.setState({
+          userBgData: res
+        });
+      }
+    });
+
+    xhr.open('GET', 'https://api.dexcom.com/v2/users/self/egvs?startDate=2020-01-01T15:30:00&endDate=2020-01-20T15:45:00');
+    xhr.setRequestHeader('authorization', `Bearer ${accessToken}`);
+
+    xhr.send(data);
   }
 
   render() {
-    const { username } = this.state;
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <h1>Dialytics</h1>
+        <button type="button" onClick={this.navToDexcomLogin}>Obtain User Authorization</button>
+        <button type="button" onClick={this.requestUserBgData}>Request Data</button>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,40 @@
+/* eslint-disable func-names */
+// import { base_url } from '../../tokens';
+
 const express = require('express');
-const os = require('os');
+const http = require('https');
+
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+const options = {
+  method: 'GET',
+  hostname: 'https://sandbox-api.dexcom.com',
+  port: null,
+  path: '/v2/users/self/egvs?startDate=2017-06-16T15:30:00&endDate=2017-06-16T15:45:00',
+  headers: {
+    authorization: 'Bearer {your_access_token}',
+  }
+};
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+const req = http.request(options, (res) => {
+  const chunks = [];
+  res.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+  res.on('end', () => {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+app.use(express.static('dist'));
+
+app.get('https://sandbox-api.dexcom.com', (req, res) => {
+  console.log('server');
+  res.send();
+});
+
+req.end();
+
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
